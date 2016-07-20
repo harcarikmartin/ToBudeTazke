1c5981014c7824d249e048f7d1dfa3024c7fd82f
harcarikmartin <martinharcarik@192.168.1.11>
2016-07-20 20:51:25 +0200

    160720 20.50

---
+++ ToBudeTazke/src/sk/tsystems/gamestudio/game/stones/core/StonesException.java
@@ -0,0 +1,12 @@
+package sk.tsystems.gamestudio.game.stones.core;
+
+public class StonesException extends RuntimeException {
+
+	public StonesException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StonesException(String message) {
+		super(message);
+	}	
+}
