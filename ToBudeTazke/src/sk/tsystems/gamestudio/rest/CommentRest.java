a66ff8705d2df1bca63eb1d10c3d3f5937436918
harcarikmartin <harcarikmartin@icloud.com>
2016-08-03 10:34:44 +0200

    160803 10.30

--- ToBudeTazke/src/sk/tsystems/gamestudio/rest/CommentRest.java
+++ ToBudeTazke/src/sk/tsystems/gamestudio/rest/CommentRest.java
@@ -9,7 +9,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-
 import sk.tsystems.gamestudio.entity.jpa.Comment;
 import sk.tsystems.gamestudio.service.jpa.CommentJpa;
 
